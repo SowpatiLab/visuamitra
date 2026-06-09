6c254c094f5065cf3b3c772d7f1ccd7d58e0c173
Siddharth K <siddharthk.jgs@gmail.com>
2026-06-09 17:31:57 +0530

    ver patch

--- src/visuamitra/__init__.py
+++ src/visuamitra/__init__.py
@@ -3,4 +3,4 @@ from .visuamitra_script import visuamitra_data_extract_stream
 
 #  debugging
 #print("!!! INIT LOADED: Exporting visuamitra_data_extract_stream")
-__version__ = "0.1.0"
+__version__ = "1.0.3"
